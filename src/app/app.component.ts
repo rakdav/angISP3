900effd56131a57a00bcecd3bd1659cd4a02da41
John Doe <gogs@fake.local>
2021-03-31 10:39:21 +0200

    4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,45 +24,44 @@ class Item{
 
 @Component({
     selector:"my-app",
-    template:'<div class="page-header">' +
-                '<h1>Список покупок</h1>' +
-             '</div>' +
-        '<div class="panel">' +
-            '<div class="form-inline">' +
-                '<div class="form-group">' +
-                    '<div class="col-md-8">' +
-                    '   <input class="form-control" [(ngModel)]="text" placeholder="Название">' +
-                    '</div>' +
-                '</div> ' +
-                '<div class="form-group">' +
-                    '<div class="col-md-6">' +
-                    '   <input class="form-control" [(ngModel)]="price" placeholder="Название">' +
-                    '</div>' +
-                '</div> '+
-                '<div class="form-group">' +
-                    '<div class="col-md-offset-2 col-md-8">' +
-                    '   <button class="btn btn-default" (click)="addItem(text,price)" )>Добавить'
-                        +'</button>' +
-                    '</div>' +
-                '</div> '+
-            '</div>'+
-        '<table class="table table-striped">' +
-        '<thead>' +
-            '<tr>' +
-                '<th>Предмер</th>' +
-                '<th>Цена</th>' +
-                '<th>Куплено</th>' +
-            '</tr>' +
-        '</thead>' +
-        '<tbody>' +
-            '<tr *ngFor="let iten of items">' +
-                '<td>{{item.purshase}}</td>' +
-                '<td>{{item.price}}</td>' +
-                '<td>{{item.purshase}}</td>' +
-            '</tr> ' +
-        '</tbody>' +
-        '</table>' +
-        '</div>'
+    template:`<div class="page-header">
+                <h1> Список покупок </h1>
+                </div>
+    <div class="panel">
+        <div class="form-inline">
+            <div class="form-group">
+                <div class="col-md-8">
+                    <input class="form-control" placeholder = "Название" />
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-md-6">
+                    <input type="number" class="form-control" placeholder="Цена" />
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-md-offset-2 col-md-8">
+                    <button class="btn btn-default">Добавить</button>
+                </div>
+            </div>
+        </div>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>Предмет</th>
+                    <th>Цена</th>
+                    <th>Куплено</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Хлеб</td>
+                    <td>100</td>
+                    <td><input type="checkbox"/></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>`
 })
 export class AppComponent{
 }
