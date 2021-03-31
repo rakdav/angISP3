ea65c7c0b548e51ed83025299a3ddbef835d093d
John Doe <gogs@fake.local>
2021-03-31 11:31:11 +0200

    6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,9 +15,9 @@ class Item{
     done:boolean;
     price:number;
 
-    constructor(purshase: string, done: boolean, price: number) {
+    constructor(purshase: string, price: number,done: boolean) {
         this.purshase = purshase;
-        this.done = done;
+        this.done = false;
         this.price = price;
     }
 }
@@ -31,17 +31,17 @@ class Item{
         <div class="form-inline">
             <div class="form-group">
                 <div class="col-md-8">
-                    <input class="form-control" placeholder = "Название" />
+                    <input class="form-control" [(ngModel)]="text"  placeholder = "Название" />
                 </div>
             </div>
             <div class="form-group">
                 <div class="col-md-6">
-                    <input type="number" class="form-control" placeholder="Цена" />
+                    <input type="number" [(ngModel)]="price" class="form-control" placeholder="Цена" />
                 </div>
             </div>
             <div class="form-group">
                 <div class="col-md-offset-2 col-md-8">
-                    <button class="btn btn-default">Добавить</button>
+                    <button class="btn btn-default" (click)="addItem(text,price)">Добавить</button>
                 </div>
             </div>
         </div>
@@ -54,14 +54,26 @@ class Item{
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>Хлеб</td>
-                    <td>100</td>
-                    <td><input type="checkbox"/></td>
+                <tr *ngFor="let item of items">
+                    <td>{{item.purshase}}</td>
+                    <td>{{item.price}}</td>
+                    <td><input type="checkbox" [(ngModel)]="item.done"/></td>
                 </tr>
             </tbody>
         </table>
     </div>`
 })
 export class AppComponent{
+    text:string;
+    price:number=0;
+    items:Item[]=[
+        {purshase:"Хлеб",done:false,price:45.0},
+        {purshase:"Масло",done:true,price:67.0},
+        {purshase:"Молоко",done:true,price:70.0},
+        {purshase:"Сыр",done:false,price:220.0}
+    ];
+    addItem(text:string,price:number):void{
+        if(text==null||text.trim()==""||price==null) return;
+        this.items.push(new Item(text,price,false));
+    }
 }
